5344c3b3ea80204a69e7295ae1cacac9fec33e4a
dwstrong5 <80288957+dwstrong5@users.noreply.github.com>
2023-06-13 10:08:32 -0700

    added 'add child' functionality

--- app.py
+++ app.py
@@ -29,8 +29,10 @@ def confirm():
                 entry = entry + str(sub_item) + ";"
             new_entry[item[0]] = entry[:-1]
     id = entries.insert_one(new_entry)
+    print(new_entry)
 
-    return render_template("confirmation.html", entry=entries.find_one(id.inserted_id))
+    #return render_template("confirmation.html", entry=entries.find_one(id.inserted_id))
+    return render_template("confirmation.html")
 
 
 
