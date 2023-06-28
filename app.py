eb27862dda8bfd157412404d27ced239be82fbd2
dwstrong5 <80288957+dwstrong5@users.noreply.github.com>
2023-06-28 12:27:49 -0700

    Reworked registration and checkin in app, fixed adding and removing children on checkin page

--- app.py
+++ app.py
@@ -12,13 +12,8 @@ entries = db.get_collection('entries')
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-@app.route("/")
-def index():
-    return render_template("register.html")
-
-@app.route("/confirm", methods=["POST"])
-def confirm():
-    data = request.form
+def generateNewEntry(data):
+    print(data)
     new_entry = dict()
     new_entry["date"] = datetime.today().strftime('%b-%d-%Y')
     for item in list(data.lists()):
@@ -27,17 +22,34 @@ def confirm():
             if(sub_item != ''):
                 entry = entry + str(sub_item) + ";"
             new_entry[item[0]] = entry[:-1]
-    id = entries.insert_one(new_entry)
-    print(new_entry)
-    res = []
-    for key in entries.find_one(id.inserted_id):
-    #for key in new_entry:
-        if "child-name" in key:
-            res.append(entries.find_one(id.inserted_id).get(key))
-            #res.append(new_entry.get(key))
+    return new_entry
     
+def getChildNames(entry):
+    res=[]
+    for key in entry:
+        if "child-name" in key:
+            res.append(entry.get(key))
+    return res
+    
+@app.route("/")
+def index():
+    return render_template("register.html")
 
-    return render_template("confirmation.html", entry=res)
+@app.route("/confirm-registration", methods=["POST"])
+def confirmRegistration():
+    id = entries.insert_one(generateNewEntry(request.form))
+    if(id):
+        return render_template("confirmation.html", entry=getChildNames(entries.find_one(id.inserted_id)))
+    else:
+        return redirect("/")
+
+@app.route("/confirm-checkin", methods=["POST"])
+def confirmCheckin():
+    id = entries.insert_one(generateNewEntry(request.form))
+    if(id):
+        return render_template("confirmation.html", entry=getChildNames(entries.find_one(id.inserted_id)))
+    else:
+        return redirect("/")
 
 @app.route("/checkin")
 def checkin():
--- static/scripts/quickCheckInAdd.js
+++ static/scripts/quickCheckInAdd.js
@@ -1,22 +1,31 @@
 const addButton = document.querySelector('.add-button')
 const remButton = document.querySelector('.fa-close')
 
+remButton.parentNode.addEventListener('click', (event) => {event.preventDefault()})
+
 getChildCount = () => {
     let count = 0;
     let id = ""
     children = document.querySelector('form').childNodes;
     for (let i = 0; i < children.length; i ++) {
         id = String(children[i].id)
-        if(id.includes('child-name')) count +=1
+        if(id.includes('child-container')) count +=1
     }
+    console.log
     return count;
 }
 
 count = getChildCount()
+console.log(count)
 addButton.addEventListener('click', (event) => {
+    console.log(count)
 
     event.preventDefault();
     count += 1;
+
+    const childContainer = document.createElement('div')
+    childContainer.id = 'child-container'
+
     const nameLabelContainer = document.createElement('div')
     nameLabelContainer.classList.add('child-label-container')
 
@@ -25,9 +34,11 @@ addButton.addEventListener('click', (event) => {
         
     const removeB = document.createElement('button')
     removeB.classList.add('btn', 'remove-button')
+    removeB.addEventListener('click', (event) => {event.preventDefault()})
 
     const x = document.createElement('i')
     x.classList.add('fa', 'fa-close')
+    x.addEventListener('click', removeItem)
     removeB.appendChild(x)
 
     nameLabelContainer.append(nameLabel, removeB)
@@ -36,11 +47,15 @@ addButton.addEventListener('click', (event) => {
     nameInput.type = "text"
     nameInput.id = `child-name-${count}`
 
-    addButton.parentElement.insertBefore(nameLabelContainer, addButton)
-    addButton.parentElement.insertBefore(nameInput, addButton)
+    childContainer.append(nameLabelContainer, nameInput)
+
+    addButton.parentElement.insertBefore(childContainer, addButton)
 })
 
+remButton.addEventListener('click', removeItem)
+
 function removeItem(event) {
-    event.target.parentElement.remove();
+    event.preventDefault();
+    event.target.parentElement.parentElement.parentElement.remove();
     count -= 1;
 }
