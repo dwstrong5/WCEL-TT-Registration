36337d1c7791e5740c5700dcf71e3d53b32a8478
dwstrong5 <80288957+dwstrong5@users.noreply.github.com>
2023-11-13 10:51:44 -0800

    working on reports page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, s
 from datetime import datetime
 import pymongo, os, json, certifi, bcrypt
 from pymongo.server_api import ServerApi
+import pandas as pd 
 
 app = Flask(__name__)
 
@@ -46,7 +47,7 @@ def formatPhone(num):
     
 def generateNewEntry(data):
     new_entry = dict()
-    new_entry["date"] = datetime.today().strftime('%b-%d-%Y')
+    new_entry["date"] = datetime.today().strftime('%m/%d/%Y')
     for item in list(data.lists()):
         curr_item = list(item)
         if curr_item[0] == 'phone' and curr_item[1]:
@@ -134,7 +135,6 @@ def login():
         else:
             if bcrypt.checkpw(password.encode('utf-8'), entry['password']):
                 session["email"] = entry["email"]
-                print("Just set session email to: " + session['email'])
                 data = list(entries.find({}).sort("date", -1).limit(10))
                 return render_template("home.html", message={"user": username}, data=data)
             else:
@@ -147,24 +147,31 @@ def register():
                 
 @app.route("/home", methods=["GET", "POST"])
 def home():
+
+    # If user is not logged in, redirect to login screen
     if "email" not in session:
         return redirect("/login")
     
+    # User is logged in, fetch data from DB
     username = session["email"]
     data=list(entries.find({}).sort("date", -1).limit(10))
-
+    print(data)
+    # Check if login submission is valid
     if request.method == "POST":
         username = request.form.get("email").strip()
         password = request.form.get("password").strip()
 
+        # If login attempt (user did not provide username or password) is invalid, return to login screen
         if not username or not password:
             return render_template("login.html", message="Invalid attempt. Please try again.")
-
+        
         entry = users.find_one({"email": username})
 
+        # If user is not registered, redirect to login screen
         if not entry:
             return render_template("login.html", message = "No account found. Please register for an account before logging in.")
 
+        # User account found, check password and log session
         else:
             # If password is correct, log user in and return homepage
             # Else, return user to login page and display incorrect password 
@@ -173,7 +180,8 @@ def home():
                 return render_template("home.html", message={"user": username}, data=data)
             else:
                 return render_template("login.html", message="Incorrect password. Please try again.")
-    # Handle GET request
+            
+    # User is logged in and stored in session, render homepage
     return render_template("home.html", message={"user": username}, data=data)
 
 @app.route("/reports", methods=["GET"])
